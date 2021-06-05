1ef7f6b08d60fbb79b3deef643266599498ec5e3
sakshi <jhasakshi1234@gmail.com>
2021-06-05 17:49:27 +0530

    For heroku

--- DCC/settings.py
+++ DCC/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5q6413v&z)2%pqm-ac^sz8_h6j^#c_4*pwj2)u3n(n8%+jeqr)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn DCC.wsgi --log-file -
